1f5b4a4b82af1a8a2728c37058e6447ee4558b7c
Mirdalan <mirdalan.p@gmail.com>
2023-05-31 08:49:58 +0300

    localization fixes

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from .models import User
 from django.utils.translation import gettext_lazy as _
 from .forms import UserForm
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class UsersListView(ListView):
@@ -13,27 +14,36 @@ class UsersListView(ListView):
     template_name = 'users/users_list.html'
 
 
-class UserCreateView(CreateView):
+class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserForm
-    template_name = 'users/user_create.html'
-    extra_context = {'title': _('Registration')}
+    template_name = 'users/user_form.html'
+    extra_context = {
+        'title': _('Registration'),
+        'button_text': _('Register'),
+        }
     success_url = reverse_lazy('log_in')
     success_message = _('User successfully registered')
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserForm
-    template_name = 'users/user_update.html'
+    template_name = 'users/user_form.html'
     success_message = _('User successfully changed')
     success_url = reverse_lazy('users_list')
     login_url = reverse_lazy('log_in')
-    extra_context = {'title': _('Update user')}
+    extra_context = {
+        'title': _('Update user'),
+        'button_text': _('Update'),
+        }
 
 
-class UserDeleteView(DeleteView):
+class UserDeleteView(SuccessMessageMixin, DeleteView):
     model = User
     template_name = 'users/user_delete.html'
     success_message = _('User successfully deleted')
     success_url = reverse_lazy('users_list')
-    extra_context = {'title': _('Delete user')}
+    extra_context = {
+        'title': _('Delete user'),
+        'button_text': _('Yes, delete'),
+    }
